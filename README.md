0d73c651c95eb3330b2559e9eb86f73c7c9609c8
Philip Schatz <253202+philschatz@users.noreply.github.com>
2020-02-11 13:46:25 -0600

    lint using xslt

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 `yarn`
 
 ## Run
-`node index.js [name]`
 
-The input file will be `[name].html` (it is very important that the filename ends in html because it determines the mimetype with which puppeteer reads and parses the file) and the output file will be `[name].out.xhtml`
+`./do-compare.bash ${path_to_xhtml_file}`
 
 An example `bad.html` and `bad.out.xhtml` have been included as a demonstration.
---
+++ do-compare.bash
@@ -0,0 +1,21 @@
+input_file=$1
+
+f_temp_base="temp-file"
+f_temp_in="./${f_temp_base}.html"
+f_temp_out="./${f_temp_base}.out.xhtml"
+f_in="./canonical.in.xhtml"
+f_out="./canonical.out.xhtml"
+
+[[ -f "${f_temp_in}" ]] && rm "${f_temp_in}"
+[[ -f "${f_temp_out}" ]] && rm "${f_temp_out}"
+[[ -f "${f_in}" ]] && rm "${f_in}"
+[[ -f "${f_out}" ]] && rm "${f_out}"
+
+xsltproc lint-input.xsl "${input_file}" > "${f_temp_in}"
+
+node index.js "${f_temp_base}"
+
+xmllint --pretty 1 "${f_temp_in}" > "${f_in}" || exit 110
+xmllint --pretty 1 "${f_temp_out}" > "${f_out}" || exit 110
+
+diff "${f_in}" "${f_out}"
